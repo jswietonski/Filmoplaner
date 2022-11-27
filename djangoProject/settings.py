6abc39ed0c6eb61ca4ab04577abd5bf39cdc238e
jswietonski <roxor39@gmail.com>
2022-11-27 12:32:08 +0100

    v1.0.4

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#k*2#(xozn_qdm#upvt&g_q4(arxf-=0)hn+&x=7=k34gm!jrh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['filmoplanerr.azurewebsites.net']
 
 STATIC_URL = '/static/'
 # Application definition
